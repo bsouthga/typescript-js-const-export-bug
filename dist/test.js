012e78d9e57619d632165ed845fc288e5673effe
Ben Southgate <bsouthga@gmail.com>
2018-02-04 11:10:02 -0500

    comment

--- dist/test.js
+++ dist/test.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Adapter = {};
 Adapter.prop = {};
+// comment this out, and it works
 Adapter.asyncMethod = async function () { };
 //# sourceMappingURL=test.js.map
